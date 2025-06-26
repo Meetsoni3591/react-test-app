8c1bcd9cba7f06188693422d740e9cc4ee5760d2
Meet Soni <210334690+Meet-tecmantras@users.noreply.github.com>
2025-06-26 14:44:45 +0530

    update

---
+++ src/App.js
@@ -0,0 +1,51 @@
+import React, { useState } from 'react';
+import { GoogleOAuthProvider } from '@react-oauth/google';
+import GoogleLoginButton from './GoogleLoginButton';
+
+function App() {
+  const [user, setUser] = useState(() => {
+    const storedUser = localStorage.getItem('gmail_user');
+    return storedUser ? JSON.parse(storedUser) : null;
+  });
+
+  const handleLoginSuccess = (data) => {
+    setUser(data);
+    localStorage.setItem('gmail_user', JSON.stringify(data));
+  };
+
+  return (
+    <GoogleOAuthProvider clientId="223161771777-1ji9dj4v6jguma0s780iqadgia1fqc4c.apps.googleusercontent.com">
+      <div>
+        {!user ? (
+          <GoogleLoginButton onLoginSuccess={handleLoginSuccess} />
+        ) : (
+          <div>
+             <p>Logged in as: {user.email}</p>
+            {/* <h2>Welcome, {user.id}</h2> */}
+            <button onClick={async () => {
+                await fetch('http://localhost:8000/send', {
+                method: 'POST',
+                headers: {
+                  'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({ user_id: user.id, email: user.email })
+                });
+            }}>
+              Send Mail 
+            </button>
+            <br />
+            <br />
+            <button onClick={() => {
+              localStorage.removeItem('gmail_user');
+              setUser(null);
+            }}>
+              Logout
+            </button>
+          </div>
+        )}
+      </div>
+    </GoogleOAuthProvider>
+  );
+}
+
+export default App;
---
+++ src/GoogleLoginButton.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import { useGoogleLogin } from '@react-oauth/google';
+import axios from 'axios';
+
+const GoogleLoginButton = ({ onLoginSuccess }) => {
+  const login = useGoogleLogin({
+    flow: 'auth-code',
+    scope: 'https://www.googleapis.com/auth/gmail.send',
+    redirect_uri: 'postmessage',  // 👈 this is the key// must match Google Cloud Console
+    onSuccess: async ({ code }) => {
+      try {
+        console.log("Authorization Code received:", code);
+        // console.log("redirection url ------> ",redirect_uri)
+        const res = await axios.post('http://localhost:8000/exchange', { code });
+        console.log("User info:", res.data);
+        onLoginSuccess(res.data); // { id, email }
+        alert("Login successful! You can now send emails.");
+      } catch (err) {
+        alert("Login failed.");
+        console.error("Exchange Error:", err.response?.data || err.message);
+      }
+    },
+    onError: () => {
+      alert('Google login failed');
+    },
+  });
+
+  return <button onClick={() => login()}>Sign in with Google</button>;
+};
+
+export default GoogleLoginButton;
