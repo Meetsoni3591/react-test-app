1dc6e7953eed8d09bd3839df5c8bf1bf96309c0e
Meet Soni <210334690+Meet-tecmantras@users.noreply.github.com>
2025-06-26 15:48:27 +0530

    K Update

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ function App() {
   };
 
   return (
-    <GoogleOAuthProvider clientId="223161771777-ceghj7lbjr7b4g5nln4g5lom0032ko60.apps.googleusercontent.com">
+    <GoogleOAuthProvider clientId="515255126078-gecft5fe7v962erlq8iiuup3afvrv0vr.apps.googleusercontent.com">
       <div>
         {!user ? (
           <GoogleLoginButton onLoginSuccess={handleLoginSuccess} />
